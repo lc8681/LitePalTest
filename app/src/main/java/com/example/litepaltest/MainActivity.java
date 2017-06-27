0d7a650ac72746f78dcfe4ace9efa893a61deb0d
momo_lichen <lc8681@gmail.com>
2017-06-27 19:08:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.litepaltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.tablemanager.Connector;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Connector.getDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setName("第一行代码（第二版）");
+                book.setAuthor("乔丹");
+                book.setPages(1024);
+                book.setPrice(35.05);
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setToDefault("price");
+                book.updateAll();
+            }
+        });
+    }
+}
